e65bc541d1e94e2410d45c8fed4087d1d73d2e6a
Ekkapob C <ekkapob@gmail.com>
2015-02-26 00:37:25 +0700

    add cookie

--- src/server.go
+++ src/server.go
@@ -7,15 +7,23 @@ import (
 	"net/http"
 )
 
+var s = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"success":true}`))
 }
 
+func Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.FormValue("name"))
+	fmt.Println(r.FormValue("password"))
+}
+
 func main() {
-	fmt.Println(securecookie.GenerateRandomKey(64))
-	fmt.Println(securecookie.GenerateRandomKey(32))
+	fmt.Println(s)
 	r := mux.NewRouter()
 	r.HandleFunc("/", Index)
+	r.HandleFunc("/login", Login).Methods("POST")
+
 	http.ListenAndServe(":8080", r)
 }
