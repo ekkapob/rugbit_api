b339154d168b1a487f7cc50d08672d1b05a13c2c
Ek <ekkapob@gmail.com>
2015-03-08 23:30:39 +0700

    db

--- src/db/db.go
+++ src/db/db.go
@@ -12,27 +12,6 @@ const (
 	DB_NAME = "rugbitdb"
 )
 
-type User struct {
-	id        int
-	username  string
-	firstname string
-	lastname  string
-}
-
-// func GetUserPwd(db *sql.DB, email string) (password string) {
-// 	_ = db.QueryRow("select password from users where username = $1 ", email).Scan(&password)
-// 	return password
-// }
-
-// func GetUserId(db *sql.DB, username string) int {
-// 	var id int
-// 	err := db.QueryRow("select id from users where username = $1", username).Scan(&id)
-// 	if err != sql.ErrNoRows {
-// 		return id
-// 	}
-// 	return 0
-// }
-
 func GetDb() *sql.DB {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PWD, DB_NAME)
 	db, err := sql.Open("postgres", dbinfo)
